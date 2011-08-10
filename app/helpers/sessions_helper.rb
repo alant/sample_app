0e03779783d5d771d337718ee2cf87e49317f16b
Alan Tang <alan.htang@gmail.com>
2011-08-09 22:18:53 -0700

    all test cases pass

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,8 +10,12 @@ end
   end
 
   def current_user=(user)
-    @current_user ||= user_from_remember_token
+    @current_user = user
   end
+
+def current_user
+  @current_user ||= user_from_remember_token
+end
 
   def signed_in?
     !current_user.nil?
@@ -21,6 +25,15 @@ end
     cookies.delete(:remember_token)
     self.current_user = nil
   end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def deny_access
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
 
   private
 
